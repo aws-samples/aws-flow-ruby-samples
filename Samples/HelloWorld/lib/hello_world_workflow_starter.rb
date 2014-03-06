299194f7110a5df28e207c71f2841e4aa6258763
Eron Hennessey <eron@abstrys.com>
2014-03-06 12:03:26 -0800

    Also reformatted workflow_starter in HelloWorld.

--- Samples/HelloWorld/lib/hello_world_workflow_starter.rb
+++ Samples/HelloWorld/lib/hello_world_workflow_starter.rb
@@ -5,7 +5,9 @@ require_relative 'utils.rb'
 require_relative 'hello_world_workflow.rb'
 
 # Get a workflow client to start the workflow
-my_workflow_client = AWS::Flow.workflow_client($swf.client, $domain) { {:from_class => "HelloWorldWorkflow"} }
+my_workflow_client = AWS::Flow.workflow_client(
+  $swf.client, $domain) { {:from_class => "HelloWorldWorkflow"} }
 
 puts "Starting an execution..."
-workflow_execution = my_workflow_client.start_execution("AWS Flow Framework for Ruby")
+workflow_execution = my_workflow_client.start_execution(
+  "AWS Flow Framework for Ruby")
