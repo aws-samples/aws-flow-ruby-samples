e14bd27f5b7d6c0450d8465e712151cd4cdb0ab8
Paritosh Mohan <paritom@amazon.com>
2014-04-02 15:56:24 -0700

    minor fixes in handle_error_workflow

--- Recipes/handle_error/lib/handle_error_workflow.rb
+++ Recipes/handle_error/lib/handle_error_workflow.rb
@@ -27,31 +27,47 @@ class HandleErrorWorkflow
   # This is the entry point for the workflow
   def handle_error_workflow
 
+    # This future will be set if an exception is encountered
+    ex = Future.new
+
     resource_id = client.allocate_resource
 
-    # If your activities or workflows are asynchronous, (using send_async), you
-    # must use error_handler, which is modeled after the begin / rescue / ensure
-    # pattern, for error handling.
+    # If your activities or workflows are asynchronous, (i.e. scheduled using 
+    # send_async), you must use error_handler, which is modeled after the 
+    # standard ruby begin/rescue/ensure, for error handling.
     error_handler do |t|
       t.begin do
         # Use the activity client to schedule the activity asynchronously.
         client.send_async(:use_resource, resource_id)
       end
-      t.rescue ActivityTaskFailedException do |e|
-        if e.cause.instance_of? ResourceNoResponseException
-          # If the exception is of type ResourceNoResponseException, run the
-          # activity to report this as a bad resource.
-          client.report_resource(resource_id) 
-        elsif e.cause.instance_of? ResourceNotAvailableException
-          # If the exception is of type ResourceNotAvailableException, run the
-          # activity to refresh the resource catalog
-          client.refresh_catalogue(resource_id)
-        else
-          # If the exception is not one of the handled types, then throw it
-          # again to be handled up the chain.
-          throw e
-        end
+      t.rescue Exception do |e|
+        # Set the future so that in the ensure step, we can call the
+        # handle_exception method.
+        ex.set(e)
       end
+      t.ensure do
+        # If the ex future was set, we call the handle_exception method with the
+        # exception and the resource_id
+        handle_exception(ex.get, resource_id) if ex.set?
+      end
+      end
+  end
+
+  # Helper method that will handle exceptions for us
+  def handle_exception(e, resource_id)
+    if e.cause.instance_of? ResourceNoResponseException
+      # If the exception is of type ResourceNoResponseException, run the
+      # activity to report this as a bad resource.
+      client.report_resource(resource_id) 
+    elsif e.cause.instance_of? ResourceNotAvailableException
+      # If the exception is of type ResourceNotAvailableException, run the
+      # activity to refresh the resource catalog
+      client.refresh_catalogue(resource_id)
+    else
+      # If the exception is not one of the handled types, then throw it
+      # again to be handled up the chain.
+      throw e
     end
+
   end
 end
