085161592000b852820ceff93c6e5eb0b262f712
Carsten Rösnick-Neugebauer <carsten.roesnick-neugebauer@tngtech.com>
2016-05-24 06:47:23 +0200

    Revise the deployments example to be a bit more Ruby idiomatic
    - If you already defined an attr_reader for an instance variable
    ``databases``, then you access it via ``databases``, too, skipping the
    then-optional ``@``.
    - You can also express the ``result = {}; so_something_with(result);
    result``-pattern via ``Enumerable#each_with_object``.
    - ``enum.map { |x| x.attrib }`` and ``enum.map(&:attrib)`` are
    equivalent, but the latter is shorter.
    - Don't use the return value of bang-methode prematurely: ``flatten!``,
    for example, returns nil if no modifications were made!

--- samples/deployment/lib/deployment_workflow.rb
+++ samples/deployment/lib/deployment_workflow.rb
@@ -68,8 +68,8 @@ class DeploymentWorkflow
     components = [databases, app_servers, web_servers, load_balancers]
 
     # Create the application stack
-    application_stack = ApplicationStack.new( {
-      components: components.map! { |t| t = t.values }.flatten!,
+    application_stack = ApplicationStack.new({
+      components: components.map!(&:values).flatten,
       frontend: frontend,
       activity_client: client
     })
@@ -81,52 +81,45 @@ class DeploymentWorkflow
 
   # Helper method used to create Database objects from a database config
   def build_databases databases
-    result = {}
-    databases.each do |database|
+    databases.each_with_object( {} ) do |database, result|
       result[database["id"].to_sym] = Database.new(database["host"])
     end
-    result
   end
 
   # Helper method used to create AppServer objects from an app server config
   def build_app_servers(app_servers, databases)
-    result = {}
-    app_servers.each do |app_server|
+    app_servers.each_with_object( {} ) do |app_server, result|
       result[app_server["id"].to_sym] = AppServer.new(
         app_server["host"], extract_keys(app_server["database"], databases))
     end
-    result
   end
 
   # Helper method used to create WebServer objects from web server config
   def build_web_servers(web_servers, databases, app_servers)
-    result = {}
-    web_servers.each do |web_server|
+    web_servers.each_with_object do |web_server, result|
       result[web_server["id"].to_sym] = WebServer.new( web_server["host"],
         extract_keys(web_server["database"], databases),
         extract_keys(web_server["app_server"], app_servers))
     end
-    result
   end
 
   # Helper method used to create LoadBalancer objects from a load balancer config
   def build_load_balancers(load_balancers, web_servers)
-    result = {}
-    load_balancers.each do |load_balancer|
+    load_balancers.each_with_object( {} ) do |load_balancer, result|
       result[load_balancer["id"].to_sym] = LoadBalancer.new( load_balancer["host"],
         extract_keys(load_balancer["web_server"], web_servers))
     end
-    result
   end
 
   # Helper method used to create frontend from a load balancer config
   def build_frontend(frontends, load_balancers)
     extract_keys(frontends, load_balancers)
-    #frontends.map {|id| load_balancers[id.to_sym] if load_balancers.key? id.to_sym}
   end
 
   def extract_keys(components, hash)
-    components.map { |id| hash[id.to_sym] if hash.key? id.to_sym }
+    components.each_with_object( [] ) do |id, values|
+      values << hash[id.to_sym] if hash.key? id.to_sym
+    end
   end
 
   # Helper method to check if Flow is replaying the workflow. This is used to
